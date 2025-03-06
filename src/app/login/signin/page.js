a32a76804b8bcfdd556a6148fe47b7a27a069a22
Programador Tic01 <bayron.trejo@montagas.com.co>
2025-03-06 16:08:41 -0500

    Se realizarón las validaciones con Yup

--- src/app/login/signin/page.js
+++ src/app/login/signin/page.js
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 import { FormikProvider, useFormik } from "formik";
-//import * as Yup from 'yup';
+import * as Yup from 'yup';
 
 
 const Signin=()=>{
@@ -13,20 +13,19 @@ const Signin=()=>{
             email:'',
             password:''
         },
+        validationSchema: Yup.object({
+          nombre:Yup.string().required('El nombre es obligatorio'),
+          apellido:Yup.string().required('El apellido es obligatorio'),
+          email:Yup.string().email('El nombre es obligatorio').required('El email es obligatorio'),
+          password:Yup.string()
+          .required('El password no puede ir vacio')
+          .min(6, 'El password debe tener al menos seis caracteres')
+        }),
         onSubmit:valores=>{
           console.log('enviando');
           console.log(valores);
         }
     });
-    /*const formik = useFormik({
-        initialValues: {
-          nombre:'d',
-          email: '',
-        },
-        onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
-        },
-      });*/
 
   return(
     <div>
@@ -38,24 +37,56 @@ const Signin=()=>{
 
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="text" placeholder="Nombre Usuario" value={formik.values.nombre} onChange={formik.handleChange}>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nombre" type="text" placeholder="Nombre Usuario" value={formik.values.nombre} onChange={formik.handleChange} onBlur={formik.handleBlur}>
                     </input>
                 </div>
+                {formik.touched.nombre && formik.errors.nombre?(
+                  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                    <p>Error</p>
+                    <p>{formik.errors.nombre}</p>
+                  </div>  
+                ):
+                null
+                }
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">Apellido</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="apellido" type="text" placeholder="Apellido Usuario" value={formik.values.apellido} onChange={formik.handleChange}>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="apellido" type="text" placeholder="Apellido Usuario" value={formik.values.apellido} onChange={formik.handleChange} onBlur={formik.handleBlur}>
                     </input>
                 </div>
+                {formik.touched.apellido && formik.errors.apellido?(
+                  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                    <p>Error</p>
+                    <p>{formik.errors.apellido}</p>
+                  </div>  
+                ):
+                null
+                }
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email Usuario" name="email" value={formik.values.email} onChange={formik.handleChange}>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email Usuario" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}>
                     </input>
                 </div>
+                {formik.touched.email && formik.errors.email?(
+                  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                    <p>Error</p>
+                    <p>{formik.errors.email}</p>
+                  </div>  
+                ):
+                null
+                }
                 <div className="mb-4">    
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password Usuario" value={formik.values.password} onChange={formik.handleChange}>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password Usuario" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}>
                     </input>
                 </div>
+                {formik.touched.password && formik.errors.password?(
+                  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                    <p>Error</p>
+                    <p>{formik.errors.password}</p>
+                  </div>  
+                ):
+                null
+                }
 
                 <input type="submit" className="bg-gray-800 w-full mt-5 p-2 text-white uppercas hover:cursor-pointer hover:bg-gray-900" value="Crear Cuenta" ></input>
             </form>    
